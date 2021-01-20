b12bd55c8c02d456ddca93e1f55ea881ac26aa97
moha <abw_anwr@hotmail.com>
2021-01-20 17:04:08 +0300

    I make two Change

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 # time_stats
+# here is a change
+# the seconds change 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
